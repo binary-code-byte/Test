2ee78ddc282e69799bf2fd1d0e2ae432932c8bc7
Binary <421249075@qq.com>
2024-09-20 11:31:40 +0800

    小明：第一次提交

--- src/main/java/com/bjpowernode/GithubappApplication.java
+++ src/main/java/com/bjpowernode/GithubappApplication.java
@@ -9,6 +9,7 @@ public class GithubappApplication {
     public static void main(String[] args) {
         SpringApplication.run(GithubappApplication.class, args);
         System.out.println("hello");
+        System.out.println("我是小明，我的第一次修改");
     }
 
 }
